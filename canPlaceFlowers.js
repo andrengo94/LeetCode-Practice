c339dd22021976b0265b1464e34992c820d67f4b
Andre Ngo <101232544+andrengo94@users.noreply.github.com>
2024-11-15 16:32:12 -0800

    Complete solution

--- canPlaceFlowers.js
+++ canPlaceFlowers.js
@@ -6,7 +6,6 @@
 var canPlaceFlowers = function(flowerbed, n) {
     for (var i = 0; i < flowerbed.length; i++) {
         if (flowerbed[i] === 0 && !flowerbed[i - 1] && !flowerbed[i + 1]) {
-            console.log(i, flowerbed[i])
             i++;
             n--;
         }
